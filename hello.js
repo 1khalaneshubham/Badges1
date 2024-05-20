0b64268d92c7d2ef98677fc51c4f6971f112a5b0
Mr_Shubham_Khalane <118313700+1khalaneshubham@users.noreply.github.com>
2024-05-20 20:57:45 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,3 +2,9 @@ console.log("Hello");
 console.log("Work in progress");
 console.log("Hello");
 console.log("Work in progress");
+// code extra add 
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML =
+"The value of z is: " + z;
